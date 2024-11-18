f7f4d6484f5ab244b4f854a631dce75d6b7222be
DuetoPark <inhye.park.dev@gmail.com>
2024-11-19 00:33:09 +0900

    🚧 [wip] feature - todos 출력

--- src/pages/todos/components/todoList/TodoList.tsx
+++ src/pages/todos/components/todoList/TodoList.tsx
@@ -1,13 +1,45 @@
+import { useEffect, useState } from "react";
 import { useTodoStore } from "../../../../store/todoStore";
+import axios from "axios";
+import { useAuthStore } from "../../../../store/authStore";
+import { Link } from "react-router-dom";
+
+interface TodoType {
+  title: string;
+  content: string;
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+}
 
 const TodoList = () => {
   const { setMode } = useTodoStore();
+  const [todoList, setTodoList] = useState<TodoType[] | null>(null);
+  const { token } = useAuthStore();
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BASE_URL}/todos`, {
+        headers: { Authorization: token },
+      })
+      .then((res) => {
+        setTodoList(res.data.data);
+      });
+  }, []);
 
   return (
     <section>
       <h3>TodoList</h3>
 
       <button onClick={() => setMode("new")}>일정 추가</button>
+
+      <ol>
+        {todoList?.map((item) => (
+          <li key={item.id}>
+            <Link to={`/todo/${item.id}`}>{item.title}</Link>
+          </li>
+        ))}
+      </ol>
     </section>
   );
 };

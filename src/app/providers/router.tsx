dcc0f3b2a85e95f7e1adf3a14dcb1a58c9d2ec8a
DuetoPark <inhye.park.dev@gmail.com>
2024-12-03 23:58:24 +0900

    🚚 [move] app router 파일 src/app > src/app/providers

--- src/app/providers/router.tsx
+++ src/app/providers/router.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
-import NotFound from "../pages/error/NotFound";
-import { authRouter } from "../features/auth/router";
-import { todosRouter } from "../features/todos/router";
+import App from "../App";
+import NotFound from "../../pages/error/NotFound";
+import { authRouter } from "../../features/auth/router";
+import { todosRouter } from "../../features/todos/router";
 
 export const router = createBrowserRouter([
   {
